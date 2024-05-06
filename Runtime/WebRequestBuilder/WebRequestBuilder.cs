21cdfbba3494a1f4334f9544cbff24c3fd3ba20e
AnkitTorenzo <31123414+AnkitTorenzo@users.noreply.github.com>
2024-05-06 19:09:33 +0530

    [WIP] Webrequest Builder - building process

--- Runtime/WebRequestBuilder/WebRequestBuilder.cs
+++ Runtime/WebRequestBuilder/WebRequestBuilder.cs
@@ -1,7 +1,9 @@
-
 using System;
+using System.Collections.Generic;
 using System.IO;
+using Codice.ThemeImages;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class WRB
 {
@@ -19,7 +21,7 @@ public class WRB
         _config = config;
     }
 
-    public static WRB CreateGetRequestBuilder(string target)
+    public static WRB CreateRequestBuilder(string target)
     {
         if (!Uri.IsWellFormedUriString(target, UriKind.RelativeOrAbsolute))
         {
@@ -43,13 +45,119 @@ public class WRB
     #endregion
 
     private string _url;
+    private string _methodName;
+
+    //Different types of payload
+    private Dictionary<string, string> dictPayload;
+    private WWWForm formPayload;
+    private string _strPayload;
+
+    private Action<string> errorHandler, responseHandler;
+
+    private bool _isBuilt = false;
+    private UnityWebRequest _request;
+
     private WRB() { }
+
+    public WRB SetMethodType(RequestMethod method)
+    {
+        _methodName = method.ToString();
+        return this;
+    }
+
+    public WRB SetDictPayload(Dictionary<string, string> payload)
+    {
+        dictPayload = payload;
+        return this;
+    }
+
+    public WRB SetWFormPayload(WWWForm payload)
+    {
+        if (_methodName == RequestMethod.NONE.ToString() || _methodName == RequestMethod.GET.ToString())
+        {
+            Debug.LogError($"WWWForm is not supported with 'RequestMethod.GET'. Please use the same method with Dictionary<string, string> as parameter, or change the RequestMethod from 'GET' to something-else");
+            Debug.LogError($"Aborting the request building chain.");
+            return null;
+        }
+
+        formPayload = payload;
+        return this;
+    }
+
+    public WRB SetStrPayload(string strPayload)
+    {
+        if (_methodName == RequestMethod.NONE.ToString() || _methodName == RequestMethod.GET.ToString())
+        {
+            Debug.LogError($"strPayload is not supported with 'RequestMethod.GET'. Please use the same method with Dictionary<string, string> as parameter, or change the RequestMethod from 'GET' to something-else");
+            Debug.LogError($"Aborting the request building chain.");
+            return null;
+        }
+
+        _strPayload = strPayload;
+        return this;
+    }
+
+    public WRB AddErrorHandler(Action<string> errorCallback)
+    {
+        errorHandler = errorCallback;
+        return this;
+    }
+
+    public WRB AddResponseHandler(Action<string> respHandler)
+    {
+        responseHandler = respHandler;
+        return this;
+    }
+
+    public WRB Build()
+    {
+        if (_isBuilt)
+        {
+            Debug.LogWarning($"This WebRequest is already built. Returning already built request.");
+            return this;
+        }
+
+        if (_methodName == RequestMethod.NONE.ToString())
+        {
+            Debug.LogError($"Hay you haven't set the type of method for your request. Please user SetMethodType() method to add/set method of your request.");
+            Debug.LogError($"Aborting the building process.");
+            return null;
+        }
+
+
+        _request = new()
+        {
+            url = _url,
+            method = _methodName.ToString(),
+        };
+        return this;
+    }
+
+    public void Send()
+    {
+        if (_request == null)
+        {
+            Debug.LogError($"It seams that you forget to build the request before sending it. Call Build() method right before calling Send() method in the builder chain.");
+            return;
+        }
+    }
 }
 
 public class WRBConfig
 {
     public string baseUrl;
     public string authToken;
+    public Action<string> defaultErrorHandler;
+    public Action<string> defaultResponseHandler;
 
     internal bool isWellFormedUrl;
+}
+
+public enum RequestMethod
+{
+    NONE,
+    GET,
+    POST,
+    PUT,
+    DELETE,
 }
